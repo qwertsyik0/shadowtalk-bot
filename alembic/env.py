9d2a351f16fc60041f8d7b6d34b5c3569dfbc0b6
Qwertsyi <qwertsyik@gmail.com>
2026-09-19 17:04:40 +0300

    fix: make Alembic Neon SSL-compatible

--- alembic/env.py
+++ alembic/env.py
@@ -51,6 +51,7 @@ async def run_async_migrations() -> None:
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
+        connect_args={"ssl": "require"},
     )
 
     async with connectable.connect() as connection:
