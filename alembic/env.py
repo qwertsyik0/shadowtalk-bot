7607c470545189025fda1fbdff99cfd256cfa868
Qwertsyi <qwertsyik@gmail.com>
2026-09-19 16:54:45 +0300

    fix: escape percent signs in Alembic database URLs

--- alembic/env.py
+++ alembic/env.py
@@ -16,7 +16,10 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 settings = get_settings()
-config.set_main_option("sqlalchemy.url", settings.sqlalchemy_database_url)
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.sqlalchemy_database_url.replace("%", "%%"),
+)
 target_metadata = Base.metadata
 
 
